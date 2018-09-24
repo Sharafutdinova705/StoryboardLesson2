d0bfeffcc6627e0a248f8a60b8dae3a48a431895
Guzel Sharafutdinova <guzelsharafutdinova@icloud.com>
2018-09-24 22:39:57 +0300

    fixed

--- NewProject/NewProject/SecondViewController.swift
+++ NewProject/NewProject/SecondViewController.swift
@@ -1,26 +1,19 @@
-//
-//  SecondViewController.swift
-//  NewProject
-//
-//  Created by Гузель on 22.09.2018.
-//  Copyright © 2018 Гузель. All rights reserved.
-//
 
 import UIKit
 
 class SecondViewController: UIViewController {
-    @IBOutlet weak var scrollinfo: UIScrollView!
-    @IBOutlet weak var phoneNumber: UILabel!
-    @IBOutlet weak var vklink: UILabel!
-    @IBOutlet weak var status: UILabel!
-    @IBOutlet weak var textFieldStatus: UITextField!
+    @IBOutlet weak var infoScrollView: UIScrollView!
+    @IBOutlet weak var phoneNumberUILabel: UILabel!
+    @IBOutlet weak var vklinkUILabel: UILabel!
+    @IBOutlet weak var statusUILabel: UILabel!
+    @IBOutlet weak var textFieldStatusUITextField: UITextField!
     
     
-    var numbers:[String] = ["89274635768", "89274656868", "89274735789", "89274098765", "89274101010"]
-    var vk:[String] = ["vk.com/catcat", "vk.com/lovecars", "vk.com/doc", "vk.com/white_cat"]
+    var numbersArray:[String] = ["89274635768", "89274656868", "89274735789", "89274098765", "89274101010"]
+    var vkLinksArray:[String] = ["vk.com/catcat", "vk.com/lovecars", "vk.com/doc", "vk.com/white_cat"]
     
     override func viewDidLoad() {
-        scrollinfo.contentSize.height = 1000
+        infoScrollView.contentSize.height = 1000
         super.viewDidLoad()
         randomize()
     }
@@ -33,19 +26,8 @@ class SecondViewController: UIViewController {
 
     func randomize() {
     
-        phoneNumber.text = numbers[Int(arc4random_uniform(UInt32(numbers.count)))]
-        vklink.text = vk[Int(arc4random_uniform(UInt32(vk.count)))]
+        phoneNumberUILabel.text = numbersArray[Int(arc4random_uniform(UInt32(numbersArray.count)))]
+        vklinkUILabel.text = vkLinksArray[Int(arc4random_uniform(UInt32(vkLinksArray.count)))]
     
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- NewProject/NewProject/ViewController.swift
+++ NewProject/NewProject/ViewController.swift
@@ -1,65 +1,58 @@
-//
-//  ViewController.swift
-//  NewProject
-//
-//  Created by Гузель on 17.09.2018.
-//  Copyright © 2018 Гузель. All rights reserved.
-//
+
 
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var name: UILabel!
-    @IBOutlet weak var status: UILabel!
-    @IBOutlet weak var profilImage: UIImageView!
-    @IBOutlet weak var cityYearsOld: UILabel!
-    @IBOutlet weak var info: UIButton!
+    @IBOutlet weak var nameUILabel: UILabel!
+    @IBOutlet weak var statusUILabel: UILabel!
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var cityYearsOldUILabel: UILabel!
+    @IBOutlet weak var infoUIButton: UIButton!
     //user's photos
-    @IBOutlet weak var photo1: UIImageView!
-    @IBOutlet weak var photo2: UIImageView!
-    @IBOutlet weak var photo3: UIImageView!
-    @IBOutlet weak var photo4: UIImageView!
-    @IBOutlet weak var ScrollView: UIScrollView!
-    @IBOutlet weak var scroll: UIScrollView!
+    @IBOutlet weak var photo1UIImageView: UIImageView!
+    @IBOutlet weak var photo2UIImageView: UIImageView!
+    @IBOutlet weak var photo3UIImageView: UIImageView!
+    @IBOutlet weak var photo4UIImageView: UIImageView!
+    @IBOutlet weak var firstScrollView: UIScrollView!
+    @IBOutlet weak var secondScrollView: UIScrollView!
     
     
     
-    var userNames:[String] = ["Amelia Dammer", "Alina Rathner", "Elis Mark", "Marie Zoline"]
-    var currentStatus:[String] = ["Online •", "Online(phone) •", "Offline", "last seen 40 minutes ago"]
-    var photos:[UIImage] = [#imageLiteral(resourceName: "girl1"), #imageLiteral(resourceName: "girl3"), #imageLiteral(resourceName: "girl4"), #imageLiteral(resourceName: "girl5")]
-    var photos2:[UIImage] = [#imageLiteral(resourceName: "girl1"), #imageLiteral(resourceName: "girl3"), #imageLiteral(resourceName: "girl4"), #imageLiteral(resourceName: "girl5"), #imageLiteral(resourceName: "img"), #imageLiteral(resourceName: "iii")]
-    var yearsOldCity:[String] = ["18 years old, NY", "23 years old, Boston", "19 years old, Moskow", "19 years old, Kazan"]
+    var userNamesArray:[String] = ["Amelia Dammer", "Alina Rathner", "Elis Mark", "Marie Zoline"]
+    var currentStatusArray:[String] = ["Online •", "Online(phone) •", "Offline", "last seen 40 minutes ago"]
+    var photosArray:[UIImage] = [#imageLiteral(resourceName: "girl1"), #imageLiteral(resourceName: "girl3"), #imageLiteral(resourceName: "girl4"), #imageLiteral(resourceName: "girl5")]
+    var photos2Array:[UIImage] = [#imageLiteral(resourceName: "girl1"), #imageLiteral(resourceName: "girl3"), #imageLiteral(resourceName: "girl4"), #imageLiteral(resourceName: "girl5"), #imageLiteral(resourceName: "img"), #imageLiteral(resourceName: "iii")]
+    var yearsOldCityArray:[String] = ["18 years old, NY", "23 years old, Boston", "19 years old, Moskow", "19 years old, Kazan"]
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        profilImage.clipsToBounds = true
-        ScrollView.contentSize.height = 1600
-        scroll.contentSize.width = 720
+        profileImageView.clipsToBounds = true
+        firstScrollView.contentSize.height = 1600
+        secondScrollView.contentSize.width = 720
         random()
-        profilImage.layer.cornerRadius = profilImage.frame.size.width / 2
-        profilImage.clipsToBounds = true
+        profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
+        profileImageView.clipsToBounds = true
         //profilImage.layer.cornerRadius = 20
         //profilImage.clipsToBounds = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     func random() {
-        profilImage.image = photos[Int(arc4random_uniform(UInt32(photos.count)))]
+        profileImageView.image = photosArray[Int(arc4random_uniform(UInt32(photosArray.count)))]
         
-        name.text = userNames[Int(arc4random_uniform(UInt32(userNames.count)))]
-        status.text = currentStatus[Int(arc4random_uniform(UInt32(currentStatus.count)))]
-        cityYearsOld.text =  yearsOldCity[Int(arc4random_uniform(UInt32(yearsOldCity.count)))]
-        photo1.image = photos2[Int(arc4random_uniform(UInt32(photos2.count)))]
-        photo2.image = photos2[Int(arc4random_uniform(UInt32(photos2.count)))]
-        photo3.image = photos2[Int(arc4random_uniform(UInt32(photos2.count)))]
-        photo4.image = photos2[Int(arc4random_uniform(UInt32(photos2.count)))]
+        nameUILabel.text = userNamesArray[Int(arc4random_uniform(UInt32(userNamesArray.count)))]
+        statusUILabel.text = currentStatusArray[Int(arc4random_uniform(UInt32(currentStatusArray.count)))]
+        cityYearsOldUILabel.text =  yearsOldCityArray[Int(arc4random_uniform(UInt32(yearsOldCityArray.count)))]
+        photo1UIImageView.image = photos2Array[Int(arc4random_uniform(UInt32(photos2Array.count)))]
+        photo2UIImageView.image = photos2Array[Int(arc4random_uniform(UInt32(photos2Array.count)))]
+        photo3UIImageView.image = photos2Array[Int(arc4random_uniform(UInt32(photos2Array.count)))]
+        photo4UIImageView.image = photos2Array[Int(arc4random_uniform(UInt32(photos2Array.count)))]
         
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
